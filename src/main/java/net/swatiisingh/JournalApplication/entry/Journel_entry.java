b538652ff5e2089b19f19c8b382ee92e002e34c8
Swati Singh <swatisingh022805@gmail.com>
2026-05-27 18:34:04 +0530

    Created all the HTTPS request and tested  on Postman

---
+++ src/main/java/net/swatiisingh/JournalApplication/entry/Journel_entry.java
@@ -0,0 +1,31 @@
+package net.swatiisingh.JournalApplication.entry;
+
+public class Journel_entry {
+    private String title;
+    private String content;
+    private long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
