fe7ad97231ad7cf8c263952d10594654b38d3cc5
Swati Singh <swatisingh022805@gmail.com>
2026-05-27 13:30:19 +0530

    Initial commit

---
+++ src/test/java/net/swatiisingh/JournalApplication/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.swatiisingh.JournalApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
